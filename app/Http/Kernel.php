cfbb39d6f13973da28dac9b68653a24f2dea6177
Matthew Chiang <matthew.chiang@caseware.com>
2021-01-07 11:49:19 -0500

    removing old password protection

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             // \ElicDev\SiteProtection\Http\Middleware\SiteProtection::class,
-            \App\Http\Middleware\SiteProtection::class,
+            // \App\Http\Middleware\SiteProtection::class,
             
         ],
 
