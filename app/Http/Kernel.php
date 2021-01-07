4be3e0d7d35cccbe12cbc761f8c081c94df32a10
Matthew Chiang <matthew.chiang@caseware.com>
2021-01-07 11:56:16 -0500

    debugging index function

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,8 +36,7 @@ class Kernel extends HttpKernel
             // UNCOMMENT BEFORE LIVE
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            // \ElicDev\SiteProtection\Http\Middleware\SiteProtection::class,
-            // \App\Http\Middleware\SiteProtection::class,
+
             
         ],
 
