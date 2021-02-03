a2b529ecb193a0cc78faf4865d14f6103be79e1a
Matthew Chiang <matthew.chiang@caseware.com>
2021-02-03 17:03:44 -0500

    styling

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,14 @@ $current_version = env("CURRENT_VERSION");
 
 
 
-Route::middleware('throttle:60|180,1')->group(function () { 
-        
+Route::middleware('throttle:60|180,1')->group(function () {
+
         // auth routes
         Auth::routes();
         Route::post('/mycwauth', 'LoginController@login');
         Route::get('/logout', 'LoginController@logout');
         Route::get('login/azure', 'Auth\LoginController@redirectToProvider');
         Route::get('login/azure/callback', 'Auth\LoginController@handleProviderCallback');
-        
-        
 });
 
 
@@ -34,45 +32,14 @@ Route::get('/new-search', 'SearchController@searchform');
 Route::get('/new-search/all', 'SearchController@all'); 
 Route::get('/new-search/{query}', 'SearchController@search');
 
-// home page - to come
-
-
-        
-
-        Route::get('/{region}/{lang}/videos/{slug?}', 'PageController@videosOverview')->name('videos');
-        // Route::get('/blog', 'PageController@blogOverview')->name('blogoverview');
-        // Route::get('/blog/{post}', 'PageController@blogDetail')->name('blogdetail');
-        Route::get('/{region}/{lang}/{slug}/context-specific-help', 'PageController@csh')->name('csh');
-        Route::get('/{region}/{lang}/{slug}/frequently-asked-questions', 'PageController@faq')->name('faq');
-        // Route::get('/{{region}/{lang}/}', 'PageController@level1')->name('home');
-        Route::get('/', 'PageController@level1')->name('home');
-        
-        
-        
-        
-        
-        
-        
-        
+// home page
+Route::get('/', 'PageController@level1')->name('home');
+          
 //Flare Content routes
 // topics
 Route::group(['middleware' => 'mycasewareauth'], function () {
 
         Route::get('/sdk/cloud/api-reference/{slug}', 'PageController@api'); 
-
         Route::get('/{product}/{version}/{category}/{param1?}/{param2?}/{param3?}/{param4?}/{param5?}/{param6?}/{param7?}', 'PageController@showTopic')->name('topic');
-        
         Route::get('/{slug}', 'PageController@level2')->name('level2');
-
-
 });
-
-
-
-
-
-
-Route::post('logemail', 'Controller@logEmail');
-
-
-Route::get('/home', 'HomeController@index')->name('home');
