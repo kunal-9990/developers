6b881ec1b933777cda9cef57d1e64730723387eb
Matthew Chiang <matthew.chiang@caseware.com>
2021-02-04 15:46:53 -0500

    removing search from login page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,11 +6,14 @@
                 <a href="/"><img class="header__logo" src="/img/CaseWare_logo_4C_horz.svg"
                         alt="CaseWare logo"></a>
             </div>
-            <div class="header__input-search-wrapper">
-                <form method="GET" action="/new-search">
-                    <input type="text" name="search" placeholder="{{ __('strings.search') }}" autocomplete="off">
-                </form>
-            </div>
+
+            @if(request()->path() !== "login")
+                <div class="header__input-search-wrapper">
+                    <form method="GET" action="/new-search">
+                        <input type="text" name="search" placeholder="{{ __('strings.search') }}" autocomplete="off">
+                    </form>
+                </div>
+            @endif
             <div class="header__nav-wrap">
                 @if(strpos(Request::url(), '/SE-Authoring/') == false)
                     @include('partials.nav')
