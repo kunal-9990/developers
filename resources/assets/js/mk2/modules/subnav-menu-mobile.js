e3f8ce110e2437f33dff7eb171381bda8d7cc247
Alex Tarapacky <alex.tarapacky@caseware.com>
2021-03-04 16:15:54 -0500

    cleanup

--- resources/assets/js/mk2/modules/subnav-menu-mobile.js
+++ resources/assets/js/mk2/modules/subnav-menu-mobile.js
@@ -1,16 +1,3 @@
-// module.exports = () => {
-//   $(document).ready(function () {
-//     let headerHeight = $('.header').height();
-//     $('.subnav .children').css("top", (1 + headerHeight) + "px")
-//     $('.subnav .parent').on("click", function() {
-//       $('.subnav').not($(this).parent()).removeClass('subnav--opened')
-//       $(this).toggleClass('parent--opened')
-//       $(this).parent().toggleClass('subnav--opened')
-//     })
-//   });
-// };
-
-
 module.exports = function() {
   var accordion = document.getElementsByClassName('accordion-tab');
   for (let i = 0; i < accordion.length; i++) {
