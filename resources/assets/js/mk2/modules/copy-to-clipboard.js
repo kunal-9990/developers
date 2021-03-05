ea462a9e92bf5547ff69621d40f1a37c185de879
Alex Tarapacky <alex.tarapacky@caseware.com>
2021-03-05 14:22:17 -0500

    cleanup

--- resources/assets/js/mk2/modules/copy-to-clipboard.js
+++ resources/assets/js/mk2/modules/copy-to-clipboard.js
@@ -1,9 +1,6 @@
 module.exports = function() {
-  console.log("preeee");  
   
   const pre = document.querySelector('pre');
-
-  pre.css('background-color', 'pink');
 
   var x = document.createElement("BUTTON");
   var t = document.createTextNode("Click me");
