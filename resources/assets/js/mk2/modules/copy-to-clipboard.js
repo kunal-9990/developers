4b2f23ef2314998656e4e62652b7075f755a1963
Alex Tarapacky <alex.tarapacky@caseware.com>
2021-03-08 11:43:05 -0500

    copy to clipboard

--- resources/assets/js/mk2/modules/copy-to-clipboard.js
+++ resources/assets/js/mk2/modules/copy-to-clipboard.js
@@ -1,39 +1,21 @@
 module.exports = function() {
 
-  const pre = document.getElementsByClassName('clipboard');
-  const clipboardBtn = document.createElement('div');
-  clipboardBtn.className = "clipboard__btn";
-  pre.appendChild(clipboardBtn);
-
-  pre.addEventListener('mouseover', () => {
-    console.log("show button");
-    clipboardBtn.classList.toggle('clipboard__btn--hover');
-  });
-
-  // for (let i = 0; i < pre.length; i++) {
-  //   accordion[i].addEventListener("click", function() {
-
-  clipboardBtn.addEventListener('click', () => {
-
-    var range, selection;
-
-    if (document.body.createTextRange) {
-      range = document.body.createTextRange();
-      range.moveToElementText(pre);
-      range.select();
-    } else if (window.getSelection) {
-      selection = window.getSelection();        
-    range = document.createRange();
-      range.selectNodeContents(pre);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    }
-    
-    try {
-      document.execCommand('copy');
-    }
-    catch (err) {
-      console.log('unable to copy text');
-    }
+  $(document).ready(function () {
+    var $button = $("<div>", {"class": "clipboard__btn"});
+    $('.clipboard').append($button);
+    $button.on("click", function() {
+      var range = document.createRange();
+      var sel = window.getSelection();
+      range.selectNodeContents($(this).parent('.clipboard')[0]);
+      sel.removeAllRanges();
+      sel.addRange(range);
+      
+      try {
+        document.execCommand('copy');
+      }
+      catch (err) {
+        console.log('unable to copy text');
+      }
+    })
   });
 }
