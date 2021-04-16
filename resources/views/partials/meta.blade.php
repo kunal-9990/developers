c065408f081921e3577f8f304fcab9cbf3e9c36b
Matthew Chiang <matthew.chiang@caseware.com>
2021-04-16 15:04:13 -0400

    updating meta

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -17,9 +17,12 @@
 <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 
 {{-- FontAwesome icons --}}
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css" integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css"
+	integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css"
+	integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css"
+	integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
 
 {{-- json ld --}}
 <script type="application/ld+json">
@@ -40,33 +43,18 @@
 {{-- Tell Google about localized versions of our pages --}}
 
 @if(env('APP_ENV') == "production")
-	{{-- google analytics --}}
-	@php
-	isset(Route::current()->parameters()["product"]) ? $product = Route::current()->parameters()["product"] : $product = '';
-	@endphp
-	@if($product == 'workingpaper' || $product  == 'audit')
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-79260220-11"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
+{{-- google analytics --}}
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-79260220-11"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
 		  function gtag(){dataLayer.push(arguments);}
 		  gtag('js', new Date());
 		
 		  gtag('config', 'UA-79260220-11');
-		</script>
-	@else
-		<script>/* <![CDATA[ */
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-97702818-1', 'auto', 'Global');
-			ga('create', 'UA-97702818-2', 'auto', 'Cloud');
-			ga('Global.send', 'pageview', { 'page': location.pathname + location.search + location.hash});
-			ga('Cloud.send', 'pageview', { 'page': location.pathname + location.search + location.hash});
-		/* ]]> */</script>
-	@endif
-	{{-- google analytics end--}}
+</script>
+
+{{-- google analytics end--}}
 @endif
 
 
