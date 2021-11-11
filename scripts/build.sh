53784ac1e7d36abe8e25eac380b20d53fd0d45ae
Matthew Chiang <matthew.chiang@caseware.com>
2021-11-11 09:51:43 -0500

    updating build script so old topic files are removed before new ones are added during publishing process

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,6 +26,7 @@ dos2unix scripts/build.sh
 echo 'Copying new content into place...'
 mkdir -p public/documentation_files/$1/$2/
 mkdir -p public/images/$1/$2/
+mv public/documentation_files/$1/$2/ /tmp
 cp -R tmp/$1/$2/* public/documentation_files/$1/$2/
 cp -R tmp/$1/$2/Content/Resources/Images/* public/images/$1/$2
 cp -R tmp/$1/$2/Content/Resources/I/* public/images/$1/$2
