15e69febcb507503e34264e84d3abf118e4253c0
mansi-bhavsar <mansi.bhavsar@caseware.com>
2022-11-28 16:47:30 -0500

    added a target url condition check

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,20 +58,19 @@ class LoginController extends Controller
 
         $request->session()->put('authenticated', true);
         $targetUrl = $request->session()->get('targetUrl', '/');
-        return redirect('/');
+        // return redirect('/');
 
-        // to be fixed
-        // if(gettype($targetUrl) == "string"){
-        //     if($targetUrl == "/documentation_files/login/undefined/OnlineOutput.xml"){
-        //                     return redirect("/");
+        if(gettype($targetUrl) == "string"){
+            if($targetUrl == "/documentation_files/login/undefined/OnlineOutput.xml"){
+                            return redirect("/");
 
-        //                 }
-        //                 else{
-        //                     return redirect($targetUrl);
-        //     }
-        // }
-        // else{
-        //     return redirect('/');
-        // }
+                        }
+                        else{
+                            return redirect($targetUrl);
+            }
+        }
+        else{
+            return redirect('/');
+        }
     }
 }
