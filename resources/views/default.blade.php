216976b47e4abc5c8121623d8640fa4c0a134c1d
Mansi Bhavsar <mansi.bhavsar@caseware.com>
2023-03-14 15:39:47 -0400

    added GTM code in default template

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -7,6 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     @yield('meta')
+  
+  	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KX6JHHN');</script>
+	<!-- End Google Tag Manager -->
 
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
     
